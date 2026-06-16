8dcbf99404e2c5e9cdc0e19f0ac27c0eb812a133
c0a2504058 <c0a2504058@edu.teu.ac.jp>
2026-06-16 14:45:09 +0900

    練習3：画面内外判定

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -13,6 +13,18 @@ DELTA = {
 }
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+def cheak_bound(rct:pg.Rect) -> tuple[bool,bool]:
+    """
+    引数：こうかとんRect or 爆弾Rect 
+    戻り値：判定結果タプル（True：画面内／False：画面外）
+    Rectオブジェクトのleft, right, top, bottomの値から画面内・外を判断する
+    """
+    yoko, tate = True, True
+    if rct.left < 0 or WIDTH < rct.right:
+        yoko = False
+    if rct.top < 0 or HEIGHT < rct.bottom:
+        tate = False
+    return yoko, tate
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -54,7 +66,14 @@ def main():
                 sum_mv[1] += mv[1]
                 
         kk_rct.move_ip(sum_mv)
+        if cheak_bound(kk_rct) != (True,True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
         bb_rct.move_ip(vx,vy)
+        yoko, tate = cheak_bound(bb_rct)
+        if not yoko:
+            vx *= -1
+        if not tate:
+            vy *= -1
         screen.blit(kk_img, kk_rct)
         screen.blit(bb_img, bb_rct)
         pg.display.update()
